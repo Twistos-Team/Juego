56dff5e69047ae5b364a007097a1d207ea27a940
WINIRAIS <101430964+WINIRAIS@users.noreply.github.com>
2022-10-27 18:07:09 -0300

    Add files via upload

---
+++ Autitos/core/src/com/mygdx/autitos/Tarro.java
@@ -0,0 +1,98 @@
+package com.mygdx.autitos;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.audio.Sound;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.MathUtils;
+import com.badlogic.gdx.math.Rectangle;
+
+
+public class Tarro {
+	   private Rectangle bucket;
+	   private Texture bucketImage;
+	   private Sound sonidoHerido;
+	   private int vidas = 3;
+	   private int puntos = 0;
+	   private int velx = 400;
+	   private boolean herido = false;
+	   private int tiempoHeridoMax=50;
+	   private int tiempoHerido;
+	   
+	   public Tarro(Texture tex, Sound ss) {
+		   bucketImage = tex;
+		   sonidoHerido = ss;
+	   }
+	   
+		public int getVidas() {
+			return vidas;
+		}
+	
+		public int getPuntos() {
+			return puntos;
+		}
+		public Rectangle getArea() {
+			return bucket;
+		}
+		public void sumarPuntos(int pp) {
+			puntos+=pp;
+		}
+		
+	
+	   public void crear() {
+
+		      bucket = new Rectangle();
+		      bucket.x = 800 / 2 - 64 / 2;
+		      bucket.y = 20;
+		      bucket.width = 40;
+		      bucket.height = 82;
+	   }
+	   public void dañar() {
+		  vidas--;
+		  herido = true;
+		  tiempoHerido=tiempoHeridoMax;
+		  sonidoHerido.play();
+	   }
+	   public void dibujar(SpriteBatch batch) {
+		 if (!herido)  
+		   batch.draw(bucketImage, bucket.x, bucket.y);
+		 else {
+		
+		   batch.draw(bucketImage, bucket.x, bucket.y+ MathUtils.random(-5,5));
+		   tiempoHerido--;
+		   if (tiempoHerido<=0) herido = false;
+		 }
+	   } 
+	   
+	   
+	   public void actualizarMovimiento() { 
+		   // movimiento desde mouse/touch
+		   /*if(Gdx.input.isTouched()) {
+			      Vector3 touchPos = new Vector3();
+			      touchPos.set(Gdx.input.getX(), Gdx.input.getY(), 0);
+			      camera.unproject(touchPos);
+			      bucket.x = touchPos.x - 64 / 2;
+			}*/
+		   //movimiento desde teclado
+		   if(Gdx.input.isKeyPressed(Input.Keys.LEFT)) bucket.x -= velx * Gdx.graphics.getDeltaTime();
+		   if(Gdx.input.isKeyPressed(Input.Keys.RIGHT)) bucket.x += velx * Gdx.graphics.getDeltaTime();
+		   if(Gdx.input.isKeyPressed(Input.Keys.UP)) bucket.y += velx * Gdx.graphics.getDeltaTime();
+		   if(Gdx.input.isKeyPressed(Input.Keys.DOWN)) bucket.y -= velx * Gdx.graphics.getDeltaTime();
+		   // que no se salga de los bordes izq y der
+		   if(bucket.x < 0) bucket.x = 0;
+		   if(bucket.x > 800 - 64) bucket.x = 800 - 64;
+		   if(bucket.y < 0) bucket.y = 0;
+		   if(bucket.y > 800-400) bucket.y = 800 - 400;
+	   }
+	    
+
+	public void destruir() {
+		    bucketImage.dispose();
+	   }
+	
+   public boolean estaHerido() {
+	   return herido;
+   }
+	   
+}
